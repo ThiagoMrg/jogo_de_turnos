da9d1d1ebcd66c1e6d0edce29ca6d12cec0e9eb2
ThiagoMrg <42078290+ThiagoMrg@users.noreply.github.com>
2024-02-21 22:15:02 -0300

    Update jogo.py
    Adicionada mecânica de jogo, onde usuário ataca e vida do inimigo decrementa

--- jogo.py
+++ jogo.py
@@ -16,6 +16,16 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNivel: {self.get_nivel()}"
 
+    def atacar(self, alvo):
+        dano = self.__nivel * 2
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida < 0:
+            self.__vida = 0
+
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
         super().__init__(nome, vida, nivel)
@@ -41,9 +51,9 @@ class Inimigo(Personagem):
 class Jogo:
     ''' Classe orquestradora do jogo '''
     def __init__(self) -> None:
-        self.heroi = Heroi(nome="Yasuo", vida=1200, nivel=6, habilidade="Ficar /10")
-        self.inimigo = Inimigo(nome="Riven", vida=1100, nivel=6, tipo="Lutador")
-    
+        self.heroi = Heroi(nome="Yasuo", vida=100, nivel=6, habilidade="Ficar /10")
+        self.inimigo = Inimigo(nome="Riven", vida=50, nivel=6, tipo="Lutador")
+
     def iniciar_batalha(self):
         """ Fazer a gestão da batalha em turnos """
         print("Iniciando batalha!")
@@ -54,6 +64,18 @@ class Jogo:
 
             input("Pressione Enter para atacar...")
             escolha = input("Escolha (1 - Ataque Normal. 2 - Ataque Especial): ")
+
+            if escolha == '1':
+                self.heroi.atacar(self.inimigo)
+            else:
+                print("Escolha inválida. Escolha novamente.")
+
+        if self.heroi.get_vida() > 0:
+            print("\nParabéns, você venceu a batalha!")
+        else:
+            print("\nVocê foi derrotado!")
+
+
 
 # Criar instância do jogo e iniciar batalha
 jogo = Jogo()
